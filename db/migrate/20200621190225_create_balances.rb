e95a6f9065741154f56ff58fa47badddb10a2820
Fábio Coelho <fabioolivc@gmail.com>
2020-06-21 16:02:54 -0300

    Add model balance

---
+++ db/migrate/20200621190225_create_balances.rb
@@ -0,0 +1,12 @@
+class CreateBalances < ActiveRecord::Migration[5.2]
+  def change
+    create_table :balances do |t|
+      t.references :account, foreign_key: true
+      t.references :deposit, foreign_key: true
+      t.references :withdraw, foreign_key: true
+      t.references :transfer, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
