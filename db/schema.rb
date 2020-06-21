00a416be0178e1c1045bb8d238576458229d9ef3
Fábio Coelho <fabioolivc@gmail.com>
2020-06-21 15:59:29 -0300

    Add model deposit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_21_185422) do
+ActiveRecord::Schema.define(version: 2020_06_21_185845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2020_06_21_185422) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
+
+  create_table "deposits", force: :cascade do |t|
+    t.bigint "account_id"
+    t.decimal "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["account_id"], name: "index_deposits_on_account_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +44,5 @@ ActiveRecord::Schema.define(version: 2020_06_21_185422) do
   end
 
   add_foreign_key "accounts", "users"
+  add_foreign_key "deposits", "accounts"
 end
