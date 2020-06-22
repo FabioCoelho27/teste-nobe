9d7dc1f16d76bcab345e5a2dce043682bf89463a
Fábio Coelho <fabioolivc@gmail.com>
2020-06-22 13:12:53 -0300

    Add list balances

--- app/models/balance.rb
+++ app/models/balance.rb
@@ -3,4 +3,30 @@ class Balance < ApplicationRecord
   belongs_to :deposit
   belongs_to :withdraw
   belongs_to :transfer
+
+  def get_type
+    if self.withdraw.present?
+      transaction_type = 'Saque'
+    elsif self.deposit.present?
+      transaction_type = 'Depósito'
+    elsif self.transfer.present?
+      transaction_type = 'Transferência'
+    else
+      transaction_type = 'Desconhecida'
+    end
+  end
+
+  def get_value
+    if self.withdraw.present?
+      value = self.withdraw.amount
+    elsif self.deposit.present?
+      value = self.deposit.amount
+    elsif self.transfer.present?
+      value = self.transfer.amount
+    else
+      value = 0
+    end
+  end
+
 end
+
