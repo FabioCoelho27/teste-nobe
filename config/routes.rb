22f1cd06392a1d0f7047118f763246cc2d1dd879
Fábio Coelho <fabioolivc@gmail.com>
2020-06-24 14:18:35 -0300

    config devise application to get

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
   resources :withdraws
   resources :deposits
   resources :accounts
-  get 'wellcome/index'
   devise_for :users
-  root to:"wellcome#index"
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to:"wellcome#index"
+  get 'wellcome/index'
 end
