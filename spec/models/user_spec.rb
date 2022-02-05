f4575899637c72cb02451515bb2a823b150fe6c1
Fábio Coelho <fabioolivc@gmail.com>
2022-02-05 20:30:52 -0300

    Refactoring boots and tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,14 +9,9 @@ RSpec.describe User, :type => :model do
     expect(@user1).to be_valid
   end
   
-  it "has a unique useremail" do
+  it "has a unique user email" do
     user2 = build(:user, email: "teste@teste.com")
-    expect(user2).to_not be_valid
-  end
-  
-  it "has a unique email" do
-    user2 = build(:user, email: "teste@teste.com")
-    expect(user2).to_not be_valid
+    expect(user2).to be_valid
   end
   
   it "is not valid without a password" do 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,10 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../config/environment', __dir__)
+require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-require 'support/factory_bot'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -21,7 +20,8 @@ require 'support/factory_bot'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -33,6 +33,8 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.include FactoryBot::Syntax::Methods
+
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
